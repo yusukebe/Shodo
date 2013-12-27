2721d1d26b0929bb5774bc7e8317b55a0c7865a9
Yusuke Wada <yusuke@kamawada.com>
2013-12-27 17:17:59 +0900

    Checking in changes prior to tagging of version 0.08.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index bf34166..7921cdf 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl extension Shodo
    
     {{$NEXT}}
    
    +0.08 2013-12-27T08:17:48Z
    +
         - move Shodo::Test::JSONRPC to "Test::Shodo::JSONRPC"
         - fixed required modules
         - update POD

--- lib/Shodo.pm
+++ lib/Shodo.pm
@@ -6,7 +6,7 @@ use Shodo::Suzuri;
 use Shodo::Hanshi;
 use Path::Tiny qw/path/;
 
-our $VERSION = "0.07";
+our $VERSION = "0.08";
 
 sub new {
     my ($class, %args) = @_;
--- t/compile.t
+++ t/compile.t
@@ -5,7 +5,7 @@ use_ok $_ for qw/
     Shodo
     Shodo::Suzuri
     Shodo::Hanshi
-    Shodo::Test::JSONRPC
+    Test::Shodo::JSONRPC
 /;
 
 done_testing;
